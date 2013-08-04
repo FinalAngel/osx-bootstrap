55907f049a5e4db42f25b0dd36faa0f67cf94fa2
FinalAngel <angelo.dini@divio.ch>
2013-08-04 23:13:06 +0200

    updates

--- osx-bootstrap-nuke.sh
+++ osx-bootstrap-nuke.sh
@@ -15,19 +15,20 @@ sudo -v
 echo '##### Uninstall OSX Bootstrap...'
 rm -rf ~/.osx-bootstrap
 
-echo '##### Uninstall Homebrew...'
-brew update
-sudo rm -rf /etc/resolver
-sudo rm -rf /usr/local/etc/dnsmasq.conf
-bash <(curl -s https://gist.github.com/mxcl/1173223/raw/a833ba44e7be8428d877e58640720ff43c59dbad/uninstall_homebrew.sh)
-
 echo '##### Uninstall oh-my-zsh...'
 uninstall_oh_my_zsh
 
 echo '##### Uninstall Settings...'
 rm -rf ~/.profile
+rm -rf ~/.zshrc
 rm -rf ~/.ssh/*
 rm -rf ~/Library/LaunchAgents/*
+
+echo '##### Uninstall Homebrew...'
+brew update
+sudo rm -rf /etc/resolver
+sudo rm -rf /usr/local/etc/dnsmasq.conf
+bash <(curl -s https://gist.github.com/mxcl/1173223/raw/a833ba44e7be8428d877e58640720ff43c59dbad/uninstall_homebrew.sh)
 
 echo '##### Note: ~/Sites will mot be removed'
 echo '##### Note: OSX Defaults will not be reset'
