3a35c648d204de26f614af5fcafd1994e8f2e184
Angelo Dini <finalangeljp@hotmail.com>
2013-08-05 11:23:23 +0200

    Update osx-bootstrap.sh

--- osx-bootstrap.sh
+++ osx-bootstrap.sh
@@ -228,6 +228,8 @@ if [[ ! -f ~/.osx-bootstrap ]]; then
     defaults write NSGlobalDomain AppleShowAllExtensions -bool true
     # Set a shorter Delay until key repeat
     defaults write NSGlobalDomain InitialKeyRepeat -int 12
+    # always show scrollbars
+    defaults write NSGlobalDomain AppleShowScrollBars -string “Always”
     # Set a blazingly fast keyboard repeat rate
     defaults write NSGlobalDomain KeyRepeat -int 0
     # Disable window animations
@@ -236,14 +238,22 @@ if [[ ! -f ~/.osx-bootstrap ]]; then
     defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
     # Disable automatic termination of inactive apps
     defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+    # Save to disk (not to iCloud) by default
+    defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
     # Expanding the save panel by default
     defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+    # Disable smooth scrolling
+    defaults write NSGlobalDomain NSScrollAnimationEnabled -bool false
     # Disable Resume system-wide
     defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
     # Display ASCII control characters using caret notation in standard text views
     defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
     # Increasing the window resize speed for Cocoa applications whether you like it or not
     defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+    # Expand print panel by default
+    defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+    # Disable “natural” (Lion-style) scrolling
+    defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
     # FINDER
     # Show dotfiles in Finder
@@ -253,7 +263,7 @@ if [[ ! -f ~/.osx-bootstrap ]]; then
     # Disable the warning when changing a file extension
     defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
     # show litsview as default
-    defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+    defaults write com.apple.Finder FXPreferredViewStyle -string "Nlsv"
     # When performing a search, search the current folder by default
     defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
     # Show absolute path in finder's title bar
@@ -282,6 +292,11 @@ if [[ ! -f ~/.osx-bootstrap ]]; then
     defaults write com.apple.dock itunes-notifications -bool true
     # Show indicator lights for open applications in the Dock
     defaults write com.apple.dock show-process-indicators -bool true
+    # Make Dock icons of hidden applications translucent
+    defaults write com.apple.dock showhidden -bool true
+    # Add a spacer to the left and right side of the Dock (where the applications are)
+    defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+    defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
     # Wipe all (default) app icons from the Dock
     # defaults write com.apple.dock persistent-apps -array
     # Reset Launchpad
@@ -342,6 +357,13 @@ if [[ ! -f ~/.osx-bootstrap ]]; then
     defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
     # show remaining battery time
     defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+    # Automatically illuminate built-in MacBook keyboard in low light
+    defaults write com.apple.BezelServices kDim -bool true
+    # Turn off keyboard illumination when computer is not used for 5 minutes
+    defaults write com.apple.BezelServices kDimTime -int 300
+    # Disable the Ping sidebar in iTunes
+    defaults write com.apple.iTunes disablePingSidebar -bool true
+    defaults write com.apple.iTunes disablePing -bool true
 
     #  killall
     for app in Finder Dock Mail Safari iTunes
