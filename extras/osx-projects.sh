04fe0a369704585ec237f1f57ddea7831dfef4c7
FinalAngel <angelo.dini@divio.ch>
2013-08-06 09:17:32 +0200

    adds proper projects support

--- extras/osx-projects.sh
+++ extras/osx-projects.sh
@@ -4,18 +4,100 @@
 clear
 
 # start bootstrap
-echo "OSX Projects 1.0.0"
-echo "------------------"
+echo "OSX Bootstrap Projects"
+echo "----------------------"
 echo ""
 
+# helper variables
 workspace=~/Sites
-organization='divio'
-project='django-cms'
-project_path=$workspace/$project
+current_path=`pwd`
+port=8000
 
-# clone and setup a testproject
-git clone git@github.com:$organization/$project.git $workspace/$project
+# project function
+function project() {
+	# cancel if no argument is given
+	[ -z $1 ] && echo '##### Warning: No project specified, use "project organization/project"' && exit
 
-virtualenv $project_path/env
+	array=(${1//\// })
+	organization=${array[0]}
+	project=${array[1]}
+	project_path=$workspace/$project
 
-# source $project_path/env/bin/activate && pip install test_requirements/django-1.5.txt
+	cd $workspace
+
+	# clone and setup a testproject
+	if [[ ! -d $project_path ]]; then
+		echo '##### Cloning Repository'
+		git clone git@github.com:$organization/$project.git
+		cd $project
+	else
+		echo '##### Updating Repository'
+		cd $project
+		git pull
+	fi
+
+	# lets figure out what to do
+	echo '##### Initializing Project'
+	echo '##### '$project
+
+	# project is django-cms
+	if [[ $project = 'django-cms' ]]; then
+		# activate virtualenv
+		[[ ! -d env ]] && virtualenv env
+		source env/bin/activate
+		# installing requirements
+		pip install -r test_requirements/django-1.5.txt
+		# python runtestserver.py -p $port -b 0.0.0.0
+	fi
+
+	# project is from divio-django-template
+	if [[ -f Makefile && -f src/settings.py ]]; then
+		# activate virtualenv
+		[[ ! -d env ]] && virtualenv env
+		source env/bin/activate
+		# check if we should init or update project
+		[[ ! -f requirements_local.txt ]] && make init
+		[[ -f requirements_local.txt ]] && make update
+		# make run PORT=$port
+	fi
+
+	# project is from divio-flask-template
+	if [[ -f Makefile && -f run.py ]]; then
+		# activate virtualenv
+		[[ ! -d env ]] && virtualenv env
+		source env/bin/activate
+		# installing requirements
+		make update
+		# make run
+	fi
+}
+
+if [[ ! $1 = 'all' ]]; then
+	project $1
+else
+	export projects='
+		aldryn/aldryn-blog
+		aldryn/aldryn-events
+		aldryn/aldryn-news
+		aldryn/aldryn-people
+		
+		FinalAngel/classjs
+		FinalAngel/classjs-plugins
+		
+		divio/divio-boilerplate
+		divio/divio-flask-template
+		divio/divio-django-template
+		divio/divio-newsletter
+		divio/divio-styleguide
+
+		divio/django-cms
+		divio/djangocms-admin-style
+		divio/djangocms-grid
+		divio/djangocms-style
+		divio/djangocms-text-ckeditor
+	'
+	for project in $projects
+	do
+		project $project
+	done
+fi
