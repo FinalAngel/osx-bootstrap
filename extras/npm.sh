43c77146bf27e709a7f7103f58724ad1b513b232
FinalAngel <angelo.dini@divio.ch>
2013-08-14 22:08:21 +0200

    add extra/npm

---
+++ extras/npm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# start bootstrap
+echo "OSX Bootstrap NPM"
+echo "------------------"
+echo ""
+
+# define helpers
+source_dir='~/.osx-bootstrap'
+source $source_dir/core/helpers.sh
+
+# install npm
+`brew list | grep node`
+[[ $1 != 0 ]]; then
+	# node installation
+	brew install node
+
+	# npm installation
+	curl https://npmjs.org/install.sh | sh
+
+	# att path
+	export NODE_PATH="/usr/local/lib/node"
+	# path for
+	# PATH="/usr/local/share/npm/bin:$PATH"
+fi
