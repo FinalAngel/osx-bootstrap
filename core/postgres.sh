757255e22d0ef2ed304953e13951defc07e3316b
FinalAngel <angelo.dini@divio.ch>
2013-09-03 08:10:45 +0200

    added sysv hack for postgres loading

--- core/postgres.sh
+++ core/postgres.sh
@@ -4,6 +4,9 @@
 source_dir=~/.osx-bootstrap
 source $source_dir/core/helpers.sh
 
+# require sudo password
+require_sudo
+
 # install postgres
 postgres=`brew list | grep postgres`
 if [[ ! $postgres ]]; then
@@ -11,6 +14,10 @@ if [[ ! $postgres ]]; then
     echo '##### Installing Formula Postgres...'
     
     PYTHON=/usr/local/bin/python brew install postgres
+
+    # update system
+    sudo sysctl -w kern.sysv.shmall=65536
+    sudo sysctl -w kern.sysv.shmmax=16777216
 
     # setup postgres
     ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
