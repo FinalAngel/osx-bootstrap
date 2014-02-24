1400c8db90f21838d0e09eb386ce84f2694b7fd3
FinalAngel <angelo.dini@divio.ch>
2014-02-24 15:19:07 +0100

    tryout simple version

--- core/helpers.sh
+++ core/helpers.sh
@@ -7,16 +7,9 @@ function require_sudo() {
     if [ $? -eq 1 ] || [ "$1" = "" ]; then
         echo ""
         echo "##### Enter Sudo Password: "
-        sudo -v
-         
+
         # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
         while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-        
-        function wait() {
-            echo -n "["; for i in {1..60}; do sleep $1; echo -n =; done; echo "]"
-        }
-
-        wait 60
     fi
 }
 
