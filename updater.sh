b84ba0d9c406ad09bf7c719ce23298233316c320
FinalAngel <angelo.dini@divio.ch>
2014-07-03 19:35:45 +0200

    added correct intendation

--- updater.sh
+++ updater.sh
@@ -1,49 +1,49 @@
 #!/bin/sh
 
 function _current_epoch() {
-  echo $(($(date +%s) / 60 / 60 / 24))
+    echo $(($(date +%s) / 60 / 60 / 24))
 }
 
 function _update_osx_update() {
-  echo "LAST_EPOCH=$(_current_epoch)" > ~/.osx-bootstrap/.osx-update
+    echo "LAST_EPOCH=$(_current_epoch)" > ~/.osx-bootstrap/.osx-update
 }
 
 function _upgrade_osx() {
-  bash ~/.osx-bootstrap/.osx-bootstrap
+    bash bootstrap
 }
 
 epoch_target=$UPDATE_OSX_DAYS
 if [[ -z "$epoch_target" ]]; then
-  # Default to old behavior
-  epoch_target=13
+    # Default to old behavior
+    epoch_target=13
 fi
 
 if [ -f ~/.osx-bootstrap/.osx-update ]
 then
-  . ~/.osx-bootstrap/.osx-update
+    . ~/.osx-bootstrap/.osx-update
 
-  if [[ -z "$LAST_EPOCH" ]]; then
-    _update_osx_update && return 0;
-  fi
-
-  epoch_diff=$(($(_current_epoch) - $LAST_EPOCH))
-  if [ $epoch_diff -gt $epoch_target ]
-  then
-    if [ "$DISABLE_UPDATE_PROMPT" = "true" ]
-    then
-      _upgrade_osx
-    else
-      echo "OSX Bootstrap would like to check for updates?"
-      echo "Type Y to update: \c"
-      read line
-      if [ "$line" = Y ] || [ "$line" = y ]; then
-        _upgrade_osx
-      else
-        _update_osx_update
-      fi
+    if [[ -z "$LAST_EPOCH" ]]; then
+        _update_osx_update && return 0;
     fi
-  fi
+
+    epoch_diff=$(($(_current_epoch) - $LAST_EPOCH))
+    if [ $epoch_diff -gt $epoch_target ]
+    then
+        if [ "$DISABLE_UPDATE_PROMPT" = "true" ]
+        then
+            _upgrade_osx
+        else
+            echo "OSX Bootstrap would like to check for updates?"
+            echo "Type Y to update: \c"
+            read line
+            if [ "$line" = Y ] || [ "$line" = y ]; then
+                _upgrade_osx
+            else
+                _update_osx_update
+            fi
+        fi
+    fi
 else
-  # create the update file
-  _update_osx_update
+    # create the update file
+    _update_osx_update
 fi
